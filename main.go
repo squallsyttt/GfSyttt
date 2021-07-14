2fa6a8dbd4f6d2a744e4cc435a024d67d91a9aac
syttt777 <390239178@qq.com>
2021-07-14 10:02:01 +0800

    setIndexFolder && setSerrverRoot

--- main.go
+++ main.go
@@ -5,16 +5,19 @@ import (
 	_ "cli_gf_proj/router"
 
 	"github.com/gogf/gf/frame/g"
-	"github.com/gogf/gf/net/ghttp"
 )
 
 func main() {
-	// g.Server().Run()
 
+	// g.Server()方法获取到一个默认的 Server对象，该方法采用单例模式设计
 	s := g.Server()
-	s.BindHandler("/", func(r *ghttp.Request) {
-        r.Response.Write("哈喽世界！")
-    })
+	// s.BindHandler("/", func(r *ghttp.Request) {
+    //     r.Response.Write("goframe bindHandler！")
+    // })
+
+	s.SetIndexFolder(true)
+	s.SetServerRoot("/usr/local/www/go_proj/cli_gf_proj/public")
+
 
 	s.Run()
 }
